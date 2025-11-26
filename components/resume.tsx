c1f0cdd9faea03d4cdf981d8d0b6d32de89b346e
Anurag <129600042+OM2309@users.noreply.github.com>
2025-11-26 09:51:55 +0530

    Change Button variant to outline in Resume component

--- components/resume.tsx
+++ components/resume.tsx
@@ -3,7 +3,7 @@ import { Link } from "lucide-react";
 
 export default function Resume() {
   return (
-    <Button size="lg" className="bg-white  text-black dark:text-white dark:bg-black ml-2 border border-gray-700">
+    <Button size="lg" variant="outline">
         <Link className="w-5 h-5"/>
       <span className="font-inter font-semibold text-base leading-[1.6]">Resume</span>
     </Button>
