bbbd74d4a7c745c1aa1e89111ba2e8720f4ea2d5
om2309 <23anuragsharma@gmail.com>
2025-12-01 02:07:01 +0530

    Contirubution fix

--- components/github-contribution.tsx
+++ components/github-contribution.tsx
@@ -1,17 +1,12 @@
 "use client";
-import { selectLastHalfYear } from "@/utils/selectlasthalfyear";
+// import { selectLastHalfYear } from "@/utils/selectlasthalfyear";
 import { GitHubCalendar } from "react-github-calendar";
 
 export default function GithubContribution() {
   return (
     <GitHubCalendar
       username="om2309"
-      transformData={selectLastHalfYear}
-      colorScheme={
-        document.documentElement.classList.contains("dark") ? "dark" : "light"
-      }
-      blockSize={7.5}
-      fontSize={14}
+      // transformData={selectLastHalfYear}
       labels={{
         totalCount: "{{count}} contributions in the last half year",
       }}
