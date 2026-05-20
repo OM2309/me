74c3637e266aa1197abe5adfba494eff2a49568c
OM <129600042+OM2309@users.noreply.github.com>
2026-05-20 09:38:30 +0530

    Modify experience details for current and previous roles
    Updated job details for the experience section, including current role and duration.

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,12 +2,28 @@ import ExperienceCard from "./experience-card";
 
 const Experience = () => {
   const companies = [
+     {
+      name: "Memorres",
+      role: "Software Engineer",
+      duration: "2026 - Present",
+      logo: "/images/octal.jpg",
+      current: true,
+      address: "Jaipur, India (Hybrid)",
+      responsibilities: [
+        "Working on various projects and features",
+        "Collaborating with cross-functional teams",
+        "Developing and maintaining software systems",
+        "Participating in code reviews and providing feedback",
+        "Contributing to the company's growth and success",
+      ],
+      technologies: ["Next.js", "React", "TypeScript", "Tailwind CSS", "Git"],
+    },
     {
       name: "Octal IT Solution",
       role: "Software Engineer",
-      duration: "2025 - Present",
+      duration: "2025 - 2026",
       logo: "/images/octal.jpg",
-      current: true,
+      current: false,
       address: "Jaipur, India (On-Site)",
       responsibilities: [
         "Working on various projects and features",
